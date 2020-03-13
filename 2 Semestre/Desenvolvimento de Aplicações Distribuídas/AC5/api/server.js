dee331d7ef6cb5f06d06a5c646293f8debd48720
nicholasferreira <nicholas_sc@live.com>
2020-03-12 22:31:25 -0300

    database in gitignore

--- 2 Semestre/Desenvolvimento de Aplicações Distribuídas/AC5/api/server.js
+++ 2 Semestre/Desenvolvimento de Aplicações Distribuídas/AC5/api/server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+const router = jsonServer.router('./database/db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
