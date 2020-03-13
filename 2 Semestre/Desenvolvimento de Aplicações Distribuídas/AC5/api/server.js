305b81faa07bcb31104ac40949499e132e0e8dcd
nicholasferreira <nicholas_sc@live.com>
2020-03-12 22:19:08 -0300

    dotenv

--- 2 Semestre/Desenvolvimento de Aplicações Distribuídas/AC5/api/server.js
+++ 2 Semestre/Desenvolvimento de Aplicações Distribuídas/AC5/api/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+const PORT = process.env.PORT
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
@@ -5,6 +7,6 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 server.use(router)
-server.listen(3000, () => {
-  console.log('JSON Server is running')
+server.listen(PORT, () => {
+  console.log(`** Server rodando na porta ${PORT} **`)
 })
