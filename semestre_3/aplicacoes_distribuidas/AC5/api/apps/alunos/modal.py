b44ce77bf3874c576d4326fc4d31b4e05b038c33
nicholasferreira <nicholas_sc@live.com>
2020-05-09 16:08:58 -0300

    ra in id

---
+++ semestre_3/aplicacoes_distribuidas/AC5/api/apps/alunos/modal.py
@@ -0,0 +1,11 @@
+class Alunos():
+  def __init__(self, id, nome):
+    self.id = id
+    self.nome = nome
+    
+  def to_dict(self):
+    disc = { 
+      'id': self.id, 
+      'nome': self.nome 
+    }
+    return disc
--- semestre_3/aplicacoes_distribuidas/AC5/api/main.py
+++ semestre_3/aplicacoes_distribuidas/AC5/api/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
 
 from apps.grupos.api import bp as bp_grupos
+from apps.alunos.api import bp as bp_alunos
 
 app = Flask('app')
 
@@ -14,5 +15,6 @@ def login():
   return render_template('login.html')
   
 app.register_blueprint(bp_grupos, url_prefix='/grupos')
+app.register_blueprint(bp_alunos, url_prefix='/alunos')
 
 app.run(host='0.0.0.0', port='8081', debug=True)
