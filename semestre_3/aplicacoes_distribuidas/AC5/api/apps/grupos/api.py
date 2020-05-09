0ecab0f7e7aaa0dab2e79b4f27892f1fff5102df
nicholasferreira <nicholas_sc@live.com>
2020-05-09 16:05:58 -0300

    alunos

--- semestre_3/aplicacoes_distribuidas/AC5/api/apps/grupos/api.py
+++ semestre_3/aplicacoes_distribuidas/AC5/api/apps/grupos/api.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, request, jsonify
+from constants import API_URL
+import requests as req
 
 bp = Blueprint('grupos', __name__)
 
 @bp.route('', methods=['GET'])
 def index():
-  grupos = []
-  return {}, 200
+  grupos = req.get(f'{API_URL}/grupos').json()
+  return jsonify(grupos), 200
 
 @bp.route('/<int:id>', methods=['GET'])
 def show(id):
