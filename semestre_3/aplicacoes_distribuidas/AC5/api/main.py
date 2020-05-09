2402e1b5338573bfc8dbc1c01703f94e9f4ab13a
nicholasferreira <nicholas_sc@live.com>
2020-05-09 14:36:38 -0300

    post login

--- semestre_3/aplicacoes_distribuidas/AC5/api/main.py
+++ semestre_3/aplicacoes_distribuidas/AC5/api/main.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask('app')
 
 @app.route('/')
 def index():
+  return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
   return render_template('login.html')
 
 app.run(host='0.0.0.0', port='8081', debug=True)
