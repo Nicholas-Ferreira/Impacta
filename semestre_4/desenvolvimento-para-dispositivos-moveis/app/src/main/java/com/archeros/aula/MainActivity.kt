1839d8fc08077733b7e8864bdcb77e68c3af582a
Nicholas Ferreira <nichoas_sc@live.com>
2020-08-26 00:01:02 -0300

    primeira aula

--- semestre_4/desenvolvimento-para-dispositivos-moveis/app/src/main/java/com/archeros/aula/MainActivity.kt
+++ semestre_4/desenvolvimento-para-dispositivos-moveis/app/src/main/java/com/archeros/aula/MainActivity.kt
@@ -2,10 +2,21 @@ package com.archeros.aula
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.login.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login)
+
+        btnLogin.setOnClickListener {
+            val email = etEmail.text
+            val password = etPassword.text
+
+
+            tvResult.text = "E-mail: $email Senha: $password"
+            Toast.makeText(applicationContext, "Salve", Toast.LENGTH_SHORT).show()
+        }
     }
 }
