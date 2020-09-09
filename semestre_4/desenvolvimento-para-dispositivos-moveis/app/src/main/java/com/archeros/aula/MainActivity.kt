30d358dc5ae8f6685ffd91e76691d199e29f8b3f
Nicholas Ferreira <nichoas_sc@live.com>
2020-09-08 22:14:05 -0300

    06 - ActionBar e Temas

--- semestre_4/desenvolvimento-para-dispositivos-moveis/app/src/main/java/com/archeros/aula/MainActivity.kt
+++ semestre_4/desenvolvimento-para-dispositivos-moveis/app/src/main/java/com/archeros/aula/MainActivity.kt
@@ -3,7 +3,13 @@ package com.archeros.aula
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.core.Method
+import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.fuel.json.responseJson
+import com.github.kittinunf.result.Result;
 import kotlinx.android.synthetic.main.login.*
 
 class MainActivity : DebugActivity() {
@@ -12,13 +18,33 @@ class MainActivity : DebugActivity() {
         setContentView(R.layout.login)
 
         btnLogin.setOnClickListener {
-            val email = etEmail.text
-            val password = etPassword.text
+            val ra = etRA.text.toString()
+            val password = etPassword.text.toString()
 
-            var intent = Intent(this, DashboardActivity::class.java)
-                .putExtra("user_email", email.toString());
-
-            startActivity(intent)
+            onLogin(ra, password) {
+                var intent = Intent(this, DashboardActivity::class.java)
+                startActivity(intent)
+                this.finish()
+            }
         }
+    }
+
+
+    fun onLogin(ra: String, password: String, callback: () -> Unit) {
+        val url = "https://account.impacta.edu.br/account/enter.php"
+        val params = listOf("desidentificacao" to ra, "dessenha" to password)
+
+        Fuel.post(url, params)
+            .responseJson { request, response, result ->
+                result.fold(success = { json ->
+                    if(json.obj().get("success") == true) {
+                        callback()
+                    }else{
+                        Toast.makeText(applicationContext, "Login ou senha inválidos, tente novamente.", Toast.LENGTH_SHORT).show()
+                    }
+                }, failure = { error ->
+                    Log.i("RequestResult", error.toString())
+                })
+            }
     }
 }
